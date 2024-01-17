2fef45d7a781ee38459bf5ba25277ee927b0e0c2
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-01-17 10:35:47 -0500

    Writting To File
    Function Logic:
    Finishing function logic for writting to a file

--- main.cpp
+++ main.cpp
@@ -5,15 +5,48 @@
 #include <vector>
 using namespace std;
 
+vector < string > getUserData() {
+	string filename = "bank-data.csv";
+	ifstream file(filename);
+	if (!file.is_open()) {
+		throw runtime_error("We are unable to open your files");
+	}
+	string line;
+	getline(file, line);
+	string value;
+	stringstream ss(line);
+	vector < string > row;
+	while(getline(ss, value, ',')) {
+		row.push_back(value);
+	}
+	return row;
+}
+
 bool checkForExistingAccount() {
 	string filename = "bank-data.csv";
 	ifstream file(filename);
-	// Check if valid data is within the file
 	if (!file.is_open()) {
 		return false;
 	} else {
-		return true;
+		vector < string > data = getUserData();
+		if (data.size() == 4) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+}
+
+bool writeToFile(string username, int pin) {
+	string filename = "bank-data.csv";
+	ofstream file(filename);
+	if (!file.is_open()) {
+		throw runtime_error("We are unable to open your files");
+		return false;
+	}
+	file << username << "," << to_string(pin) << ",$0.00" << ",d - $0.00";
+	file.close();
+	return true;
 }
 
 void createAccount() {
@@ -56,6 +89,18 @@ void createAccount() {
 			}
 		}
 	}
+	bool fileWrite = false;
+	try {
+	 fileWrite = writeToFile(username, pin);
+	} catch (const exception& e) {
+		cout << e.what() << endl << "We are so sorry about this inconvenience. But we cannot create an account for you at this time. We are working on fixing the issue immediately. Please come back later." << endl;
+	}
+	if (fileWrite) {
+		cout << "Your new username is " << username << endl;
+		cout << "Your new pin is " << pin << endl;
+	} else {
+		cout << endl << "We were unable to create you account. Please try again later." << endl;
+	}
 }
 
 bool validatePin(int pin) {
@@ -81,24 +126,6 @@ bool validatePin(int pin) {
 	}
 	return true;
 }
-
-vector < string > getUserData() {
-	string filename = "bank-data.csv";
-	ifstream file(filename);
-	if (!file.is_open()) {
-		throw runtime_error("We are unable to open your files");
-	}
-	string line;
-	getline(file, line);
-	string value;
-	stringstream ss(line);
-	vector < string > row;
-	while(getline(ss, value, ',')) {
-		row.push_back(value);
-	}
-	return row;
-}
-
 
 int getPin() {
 	cout << "*** Bank of CPP ***" << endl << endl;
@@ -153,6 +180,7 @@ int main() {
 	while (!accountExists) {
 		try {
 			createAccount();
+			accountExists = true;
 		} catch (const exception& e) {
 			cout << e.what() << endl << "We are so sorry about this inconvenience. But we cannot create an account for you at this time. We are working on fixing the issue immediately. Please come back later." << endl;
 		}
